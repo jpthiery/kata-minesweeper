f1208b9ec6f4ba736abd71f86137f3ecf27cfa94
jpthiery <jpthiery@xebia.fr>
2018-05-24 16:47:51 +0200

    Commit step afeter 1 hour

--- src/main/kotlin/fr/xebia/jpthiery/kata/minesweepper/MineSweeper.kt
+++ src/main/kotlin/fr/xebia/jpthiery/kata/minesweepper/MineSweeper.kt
@@ -2,8 +2,93 @@ package fr.xebia.jpthiery.kata.minesweepper
 
 class MineSweeper {
 
-    fun hello(): String {
-        return "Hello"
+    companion object {
+        fun process(input: String): String {
+            val description = Description.create(input)
+            var res = "Field #1:\n"
+            for (i in 1..description.nbRow) {
+                for (j in 1..description.nbColumn) {
+                    when (description.plan[i - 1][j - 1]) {
+                        Slot.EMPTY -> res += defineNumberMineInNeighbour(description, i - 1, j - 1)
+                        Slot.MINE -> res += "*"
+                    }
+                    if (j == description.nbColumn)
+                        res += "\n"
+                }
+
+            }
+            return res
+        }
+
+        private fun defineNumberMineInNeighbour(description: Description, row: Int, column: Int): String {
+            var nbMine = description.getSlotValue(row - 1, column - 1)
+            nbMine += description.getSlotValue(row - 1, column)
+            nbMine += description.getSlotValue(row - 1, column + 1)
+
+            nbMine += description.getSlotValue(row, column - 1)
+            nbMine += description.getSlotValue(row, column + 1)
+
+            nbMine += description.getSlotValue(row + 1, column - 1)
+            nbMine += description.getSlotValue(row + 1, column)
+            nbMine += description.getSlotValue(row + 1, column + 1)
+            return nbMine.toString()
+        }
+
+
+    }
+
+}
+
+enum class Slot {
+    EMPTY,
+    MINE
+}
+
+data class Description(val nbRow: Int, val nbColumn: Int, val plan: Array<Array<Slot>>) {
+
+    companion object {
+        fun create(input: String): Description {
+            val lines = input.split("\n")
+            val description = lines[0]
+            val descriptionSplitted = description.split(' ')
+            val nbRow = Integer.parseInt(descriptionSplitted[0])
+            val nbCollumns = Integer.parseInt(descriptionSplitted[1])
+
+            var plan = Array(nbRow, { Array(nbCollumns, { Slot.EMPTY }) })
+
+            for (i in 2..(lines.size - 1)) {
+                val line = lines[i - 1]
+                if (line.startsWith(".") or line.startsWith("*")) {
+                    val lineSplitted = line
+                            .replace("\n", "")
+                            .replace(" ", "")
+                            .toCharArray()
+                    for (j in 1..(lineSplitted.size)) {
+                        val value = lineSplitted[j - 1]
+                        plan[i - 2][j - 1] = defineSlot(value)
+                    }
+                }
+            }
+
+            return Description(nbRow, nbCollumns, plan)
+        }
+
+        private fun defineSlot(input: Char): Slot {
+            when (input) {
+                '.' -> return Slot.EMPTY
+                '*' -> return Slot.MINE
+                else -> throw RuntimeException("Unable to determinate slot type $input")
+            }
+        }
+    }
+
+    fun getSlotValue(row: Int, column: Int): Int {
+        if (row < 0 || column < 0 || row >= nbRow || column >= nbColumn)
+            return 0
+        when (plan[row][column]) {
+            Slot.EMPTY -> return 0
+            Slot.MINE -> return 1
+        }
     }
 
 }
--- src/test/kotlin/fr/xebia/jpthiery/kata/minesweepper/MineSweeperTest.kt
+++ src/test/kotlin/fr/xebia/jpthiery/kata/minesweepper/MineSweeperTest.kt
@@ -1,15 +1,82 @@
 package fr.xebia.jpthiery.kata.minesweepper
 
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import  org.assertj.core.api.Assertions.*
 
 internal class MineSweeperTest {
 
     @Test
-    fun say_hello() {
-        val swipper = MineSweeper()
-        val result = swipper.hello()
-        assertThat(result).isEqualTo("Hello")
+    fun i_could_create_a_2x2_playground_without_mine() {
+        val input = "2 2\n" +
+                ". .\n" +
+                ". .\n"
+        val playGround = MineSweeper.process(input)
+
+        assertThat(playGround).isEqualTo("Field #1:\n" +
+                "00\n" +
+                "00\n")
+    }
+
+    @Test
+    fun i_could_create_a_3x3_playground_without_mine() {
+        val input = "3 3\n" +
+                ". . .\n" +
+                ". . .\n" +
+                ". . .\n"
+        val playGround = MineSweeper.process(input)
+
+        assertThat(playGround).isEqualTo("Field #1:\n" +
+                "000\n" +
+                "000\n" +
+                "000\n")
+    }
+
+    @Test
+    fun i_could_create_a_3x4_playground_without_mine() {
+        val input = "3 4\n" +
+                ". . . .\n" +
+                ". . . .\n" +
+                ". . . .\n"
+        val playGround = MineSweeper.process(input)
+
+        assertThat(playGround).isEqualTo("Field #1:\n" +
+                "0000\n" +
+                "0000\n" +
+                "0000\n")
+    }
+
+    @Test
+    fun i_could_create_a_1x1_playground_with_1_mine() {
+        val input = "1 1\n" +
+                "*\n"
+        val playGround = MineSweeper.process(input)
+
+        assertThat(playGround).isEqualTo("Field #1:\n" +
+                "*\n")
+    }
+
+    @Test
+    fun i_could_create_a_1x2_playground_with_1_mine() {
+        val input = "1 2\n" +
+                "* .\n"
+        val playGround = MineSweeper.process(input)
+
+        assertThat(playGround).isEqualTo("Field #1:\n" +
+                "*1\n")
+    }
+
+    @Test
+    fun i_could_create_a_3x5_playground_with_3_mines() {
+        val input = "3 5\n" +
+                "**...\n" +
+                ".....\n" +
+                ".*...\n"
+        val playGround = MineSweeper.process(input)
+
+        assertThat(playGround).isEqualTo("Field #1:\n" +
+                "**100\n" +
+                "33200\n" +
+                "1*100\n")
     }
 
 }
