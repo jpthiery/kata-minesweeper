61e25dd42dd53b5c48fea204e76f66efe957c6e6
jpthiery <jpthiery@xebia.fr>
2018-05-29 15:57:45 +0200

    :boom: Result of BBL done 29 may 2018

--- src/main/java/fr.xebia.jpthiery.kata.minesweeper/fr/xebia/jpthiery/kata/minesweeper/MineSweeper.java
+++ src/main/java/fr.xebia.jpthiery.kata.minesweeper/fr/xebia/jpthiery/kata/minesweeper/MineSweeper.java
@@ -2,8 +2,36 @@ package fr.xebia.jpthiery.kata.minesweeper;
 
 public class MineSweeper {
 
-    public static String sayHello() {
-        return "Hello";
+    public Plan feed(String input) {
+        return new Plan(input);
+    }
+
+    public class Plan {
+
+        private String input;
+        private static final int UN = 1;
+        private static final int DEUX = 2;
+        private static final String STAR = "*";
+
+        public Plan(String input) {
+            this.input = input;
+        }
+
+        public int numberRows() {
+            if (input.contains("\n")) {
+                return DEUX;
+            }
+
+            return UN;
+        }
+
+        public boolean hasMine() {
+            return input.contains(STAR);
+        }
+
+        public int numberMines() {
+            return input.split("\\*").length;
+        }
     }
 
 }
--- src/test/java/fr.xebia.jpthiery.kata.minesweeper/fr/xebia/jpthiery/kata/minesweeper/MineSweeperTest.java
+++ src/test/java/fr.xebia.jpthiery.kata.minesweeper/fr/xebia/jpthiery/kata/minesweeper/MineSweeperTest.java
@@ -1,14 +1,106 @@
 package fr.xebia.jpthiery.kata.minesweeper;
 
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class MineSweeperTest {
 
     @Test
-    public void say_hello_works_fine() {
-        var result = MineSweeper.sayHello();
-        Assertions.assertThat(result).as("say Hello").isEqualTo("Hello");
+    public void not_null() {
+
+        //  Given
+
+        var input = ". .";
+        var minesweeper = new MineSweeper();
+
+        //  When
+
+        var result = minesweeper.feed(input);
+
+        //  Then
+
+        assertThat(result).isNotNull();
+
     }
+
+    @Test
+    public void retrieve_number_of_rows() {
+        //  Given
+        var input = ". .";
+
+        var minesweeper = new MineSweeper();
+
+        //when
+        var result = minesweeper.feed(input);
+
+        //Then
+        assertThat(result.numberRows()).isEqualTo(1);
+
+    }
+
+    @Test
+    public void retrieve_correct_number_of_rows() {
+        //  Given
+        var input = ". .\n" +
+                ". .";
+
+        var minesweeper = new MineSweeper();
+
+        //when
+        var result = minesweeper.feed(input);
+
+        //Then
+        assertThat(result.numberRows()).isEqualTo(2);
+
+    }
+
+    @Test
+    public void retrieve_one_mine() {
+        //  Given
+        var input = "* .";
+
+        var minesweeper = new MineSweeper();
+
+        //when
+        var result = minesweeper.feed(input);
+
+        //Then
+        assertThat(result.hasMine()).isTrue();
+
+    }
+
+    @Test
+    public void retrieve_3mines_2_rows() {
+        //  Given
+        var input = "* .\n" +
+                "* *";
+
+        var minesweeper = new MineSweeper();
+
+        //when
+        var result = minesweeper.feed(input);
+
+        //Then
+        assertThat(result.numberMines()).isEqualTo(3);
+
+    }
+
+    @Test
+    public void retrieve_2mines_2_rows() {
+        //  Given
+        var input = "* .\n" +
+                ". *";
+
+        var minesweeper = new MineSweeper();
+
+        //when
+        var result = minesweeper.feed(input);
+
+        //Then
+        assertThat(result.numberMines()).isEqualTo(2);
+
+    }
+
 
 }
